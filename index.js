59fa2918007a2244c2849c448032d44f4884660d
hgoutali <hh88remp@yahoo.fr>
2022-09-29 17:06:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("WORKING !!!");
+});
+
+app.listen(process.env.PORT || 5000);
